6d15714b1d947ef83c21499e0d8e462e10979cbd
Chris Muthig <camuthig@gmail.com>
2025-10-16 17:27:03 -0600

    Add Django 5.1 and 5.2 support

--- setup.py
+++ setup.py
@@ -99,6 +99,8 @@ setup(
         'Framework :: Django :: 3.2',
         'Framework :: Django :: 4.2',
         'Framework :: Django :: 5.0',
+        'Framework :: Django :: 5.1',
+        'Framework :: Django :: 5.2',
     ],
 
     # What does your project relate to?
